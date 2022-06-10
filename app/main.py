1c388cc224ba3676cbb163d78aba6d9680f7f4a9
Xewus <xewuss@yandex.ru>
2022-06-10 14:42:55 +0300

    2.0.6 disabled db for tests

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 
 from app.api import main_router
 from app.core.config import settings
-from app.core.init_db import create_first_superuser
+# from app.core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
